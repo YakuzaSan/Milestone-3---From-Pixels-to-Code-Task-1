5ec746086e982f48d4e287d6935776e71daeeef2
Yakuzan <lin.yihang@outlook.de>
2025-11-15 10:39:21 +0100

    Initial commit

---
+++ bin/milestone3_task1.dart
@@ -0,0 +1,5 @@
+import 'package:milestone3_task1/milestone3_task1.dart' as milestone3_task1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${milestone3_task1.calculate()}!');
+}
---
+++ test/milestone3_task1_test.dart
@@ -0,0 +1,8 @@
+import 'package:milestone3_task1/milestone3_task1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
